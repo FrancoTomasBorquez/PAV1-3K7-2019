31319350a011fa98743f5e5fec87bad1b3b1de50
FrancoTomasBorquez <francocab98@outlook.com>
2019-08-18 23:25:23 -0300

    RAMA 1 CON SUS ARCHIVOS

---
+++ Alumno/Alumno/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alumno
+{
+    public partial class A : Form
+    {
+        public A()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
